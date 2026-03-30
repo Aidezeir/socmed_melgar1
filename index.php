90799459ee215fa0410f4c39fb9bd06af3279d1c
AIDEZEIR MELGAR <aidezeirpelderosmelgar@gmail.com>
2026-03-30 15:31:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include "views/header.php";
+?>
+        <div class="reg-form">
+            <p class="title">REGISTRATION</p>
+            <form method="POST" action="models/register_account.php">
+                <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="account@web.com" >
+                <label for="firstname">First Name</label>
+                    <input type="text" name="firstname" id="firstname" placeholder="aideng" >
+                <label for="lastname">Last Name</label>
+                    <input type="text" name="lastname" id="lastname" placeholder="melgar" >
+                <label for="password">Password</label>
+                    <input type="password" name="password" id="password" >
+                <label for="cpassword">Confirm Password</label>
+                    <input type="password" name="cpassword" id="cpassword">
+                <input type="submit" value="Signup" >
+             
+                
+            </form>          
+        </div>
+   
+<?php
+include "views/footer.php";
